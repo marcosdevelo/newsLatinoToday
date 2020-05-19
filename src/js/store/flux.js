48188c106dbe48c88c22bc25083a46a3d102b576
Marcos Gomez <mdgdevelop@gmail.com>
2020-05-19 18:01:13 +0000

    images

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -112,21 +112,21 @@ const getState = ({ getStore, getActions, setStore }) => {
 						"En el inicio de la fase pública de la investigación para el impeachment de Trump en el Congreso comparecieron dos veteranos diplomáticos: William Taylor, actualmente encargado de negocios en Ucrania, y George Kent, subsecretario adjunto de Asuntos Europeos y Euroasiáticos.",
 					date: "Marzo 7, 2020",
 					image:
-						"https://scontent-atl3-1.xx.fbcdn.net/v/t1.15752-9/p1080x2048/92306564_839142039926134_179096867773087744_n.jpg?_nc_cat=103&_nc_sid=b96e70&_nc_ohc=u07YEchn3hQAX_0-I3o&_nc_ht=scontent-atl3-1.xx&_nc_tp=6&oh=a403580314758589adf37036a40e3344&oe=5EB242E3",
+						"https://scontent.fmia1-1.fna.fbcdn.net/v/t1.15752-9/p1080x2048/92306564_839142039926134_179096867773087744_n.jpg?_nc_cat=103&_nc_sid=b96e70&_nc_oc=AQmA6YcTtim2LI0JTU2NbQsr3xGVgO_JVxkaAJrhcqhIB-0PX3zD_iUrOuadmrd1yy2o4kbjKOpC716hHtoK4Ri3&_nc_ht=scontent.fmia1-1.fna&_nc_tp=6&oh=548e514fc03d3c81cc7a07781f409665&oe=5EE9A1E3",
 					type: "COMUNIDAD",
 					title: "Comunidad Vida Nueva: Mi prójimo es cualquiera que tenga necesidad",
 					image2:
-						"https://scontent.fmia1-1.fna.fbcdn.net/v/t1.15752-9/p1080x2048/92635390_647216839166745_5920744303096758272_n.jpg?_nc_cat=108&_nc_sid=b96e70&_nc_oc=AQlGGU2E05uOa09nVshapbIz8ub-kNFP4R3Iu9bpm4Y_4ZLutiuJbjCf20nY-9Q83S5GWeRmEq7TqnEr-0oVgXs0&_nc_ht=scontent.fmia1-1.fna&_nc_tp=6&oh=1fe788ee7f40b07849b7f712e32d2f61&oe=5EB2D4D0",
+						"https://scontent.fmia1-1.fna.fbcdn.net/v/t1.15752-9/p1080x2048/92565245_739376166596623_627437994017554432_n.jpg?_nc_cat=106&_nc_sid=b96e70&_nc_oc=AQmFdu2GgNzErmvvO2XwuRHRBm2avrDvrzhfo-onQrIoKUsK4bvkChRfgnGTqbXDhw-QqIS3--4nyWiI-kkhQytG&_nc_ht=scontent.fmia1-1.fna&_nc_tp=6&oh=f7d95f7215c8201072fec24620297b21&oe=5EE7EC35",
 					image3:
-						"https://scontent.fmia1-1.fna.fbcdn.net/v/t1.15752-9/92597945_520044212269866_166655717611143168_n.jpg?_nc_cat=103&_nc_sid=b96e70&_nc_oc=AQkzUP6gzrtcdObrxfRy13Ob18n0fdXezMzVcH-oA21c1gjgsV1_Gq_-F0qPJ3lEP-C37Qsm4ujBF7O0rnfzG3ZT&_nc_ht=scontent.fmia1-1.fna&oh=eed8bbcbc2e76c4f7c90c3730dc55e2c&oe=5EB572E1",
+						"https://scontent.fmia1-1.fna.fbcdn.net/v/t1.15752-9/92597945_520044212269866_166655717611143168_n.jpg?_nc_cat=103&_nc_sid=b96e70&_nc_oc=AQnBV4sHE9_38BJEfO_nlhqHUIJnnhMYcOmG8UcD98VJBOguhPCn2iKcb-K8lLTFzg1bVUm3QBkP7RHTTdZ8CdW7&_nc_ht=scontent.fmia1-1.fna&oh=5db17b23d797549996091f186a814d64&oe=5EE8DD61",
 					image4:
-						"https://scontent.fmia1-2.fna.fbcdn.net/v/t1.15752-9/p1080x2048/92822894_252715605905429_2457753940888911872_n.jpg?_nc_cat=111&_nc_sid=b96e70&_nc_oc=AQmzeel_I8xOpQ0UgYGJSvkF71GLxJHdHkPYw9KmJdd5Hw59uAD3hH_Ncwjk-KN_To3GzZMCXXDEuLckfiv_NAb0&_nc_ht=scontent.fmia1-2.fna&_nc_tp=6&oh=391d133541d865bd91f09360272979da&oe=5EB533F7",
+						"https://scontent.fmia1-1.fna.fbcdn.net/v/t1.15752-9/p1080x2048/92829787_2448334698599944_6087941654738632704_n.jpg?_nc_cat=102&_nc_sid=b96e70&_nc_oc=AQni_o8BDw0iXJiwwDynnt9Igk1oOMxhwmJT3473osFQmEl6EyjVvSN7VKXJPup2GpkY6jCYc21fjFbKSeoD-wmZ&_nc_ht=scontent.fmia1-1.fna&_nc_tp=6&oh=a4b611a3751511d2a7dfd59f3dd9b779&oe=5EEAF9EA",
 					image5:
-						"https://scontent.fmia1-1.fna.fbcdn.net/v/t1.15752-9/p1080x2048/92281981_641798286398838_3601479538262933504_n.jpg?_nc_cat=102&_nc_sid=b96e70&_nc_oc=AQlWyTjw8tGGElwfP_A6UvXJsncarB2UhQUPOW5YyI4M7LDSPIT_kHe8_s_aoF_SG2uOuFX_fpWQY9N0CN-zz2hN&_nc_ht=scontent.fmia1-1.fna&_nc_tp=6&oh=d1373db724198e801adaabcb3fe633ba&oe=5EB3BC9A",
+						"https://scontent.fmia1-2.fna.fbcdn.net/v/t1.15752-9/p1080x2048/92822894_252715605905429_2457753940888911872_n.jpg?_nc_cat=111&_nc_sid=b96e70&_nc_oc=AQlXeO1KHY6A5CooDtLxliVW3CMbyW41zFg8gKcbbM8lcln1tH-37e8xZKsnvBWfVtODAKqVwdQRrHIJRWZ1kOt5&_nc_ht=scontent.fmia1-2.fna&_nc_tp=6&oh=a02d663304280a000999b6bbee6c7b2b&oe=5EE89E77",
 					image6:
-						"https://scontent.fmia1-2.fna.fbcdn.net/v/t1.15752-9/p1080x2048/92218078_206826704077437_760663378413748224_n.jpg?_nc_cat=104&_nc_sid=b96e70&_nc_oc=AQkMJv1-glUr3kto7mO0_wvV6V-z1-5-MMQPJDh2-vZeISRbPaB3AzcvCiDA-rBp7s2QVRMYoUzL5mZsVTK6vuHx&_nc_ht=scontent.fmia1-2.fna&_nc_tp=6&oh=b02d4548c05b9d511de5e7374744dc3a&oe=5EB249C2",
+						"https://scontent.fmia1-1.fna.fbcdn.net/v/t1.15752-9/p1080x2048/92281981_641798286398838_3601479538262933504_n.jpg?_nc_cat=102&_nc_sid=b96e70&_nc_oc=AQnZrpPwYL5jsEP3RvC7Gx28G3PdOnc6AGleY2fqukdg-XeNZQM9FxBmvkgELBu6Er5zvcTJXpcRNJaYY7cBR6V6&_nc_ht=scontent.fmia1-1.fna&_nc_tp=6&oh=636deaf76cc6a614d8b5355ccf196517&oe=5EEB1B9A",
 					image7:
-						"https://scontent.fmia1-1.fna.fbcdn.net/v/t1.15752-9/p1080x2048/92565245_739376166596623_627437994017554432_n.jpg?_nc_cat=106&_nc_sid=b96e70&_nc_oc=AQl_dPt4mTvghHyioZVzootMz6HYWC9I-22opuR2KOnPEZrQaea3UT6lZTt3Euu3cdNfVPB_B8Qf-Z9oSWKlSMFo&_nc_ht=scontent.fmia1-1.fna&_nc_tp=6&oh=913c47671a9f16f07085b20a09fe1cbe&oe=5EB481B5"
+						"https://scontent.fmia1-2.fna.fbcdn.net/v/t1.15752-9/p1080x2048/92218078_206826704077437_760663378413748224_n.jpg?_nc_cat=104&_nc_sid=b96e70&_nc_oc=AQlK3miewghXyyK_ekpEaljoKYdFupCnmkc4RJGj0-FwKV1-JuXbyvudwhqd5TMllVen2ECZdTKWCFtBq0jDQy_d&_nc_ht=scontent.fmia1-2.fna&_nc_tp=6&oh=8c1fe04510867d93ec3c6c43ab28232e&oe=5EE9A8C2"
 				}
 			],
 			comunidad3: [
